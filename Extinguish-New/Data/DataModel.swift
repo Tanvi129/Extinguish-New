00ec7e302a30572990cad80a60f403c4bfd80aae
Tanvi129 <tanvigupta129@gmail.com>
2023-03-07 14:50:32 +0530

    Data Model Defined

--- Extinguish-New/Data/DataModel.swift
+++ Extinguish-New/Data/DataModel.swift
@@ -5,26 +5,45 @@
 //  Created by Thrishalini on 07/03/23.
 //
 
-import Foundation
+
 
 import Foundation
+import UIKit
 
 class DataModel {
     
     var auditorList = [
-        Auditor(empID: "12345", name: "Tanvi Gupta", phoneNumber: "9557146955", email: "tanvigupta129@gmail.com"),Auditor(empID: "12345", name: "Vansh Agarwal", phoneNumber: "9557146955", email: "tanvigupta129@gmail.com"),Auditor(empID: "12345", name: "Pratham Pandey", phoneNumber: "9557146955", email: "tanvigupta129@gmail.com"),Auditor(empID: "12345", name: "Thrishalini", phoneNumber: "9557146955", email: "tanvigupta129@gmail.com")
+        Auditor(
+            profilePic:"profilepic1",
+                empID: "12345",
+                name: "Tanvi Gupta",
+                phoneNumber: "9557146955",
+                email: "tanvigupta129@gmail.com",
+                location: "None"
+                ),
+        Auditor(profilePic: "profilepic1",
+                empID: "12345",
+                name: "Vansh Agarwal",
+                phoneNumber: "9557146955",
+                email: "tanvigupta129@gmail.com",
+                location: "None"
+               ),
+        Auditor(profilePic: "profilepic1",
+                empID: "12345",
+                name: "Pratham Pandey",
+                phoneNumber: "9557146955",
+                email: "tanvigupta129@gmail.com",
+                location: "None"
+               ),
+        Auditor(profilePic: "profilepic1",
+                empID: "12345",
+                name: "Thrishalini",
+                phoneNumber: "9557146955",
+                email: "tanvigupta129@gmail.com",
+                location: "None"
+               )
     ]
     
-    var taskSummaryBasicDatas : [taskSummaryBasicData] =
-    [taskSummaryBasicData(taskID: "zxzabsd",priority: "Immediate",location: "Nungumbakam"),
-     taskSummaryBasicData(taskID: "3vXrXv",priority: "Normal",location: "Chengalpet"),
-     taskSummaryBasicData(taskID: "4yyy5if",priority: "Normal",location: "Triplicane"),
-     taskSummaryBasicData(taskID: "dy65vko",priority: "Normal",location: "Mylapore"),
-     taskSummaryBasicData(taskID: "dy65vko",priority: "Normal",location: "Mylapore"),
-     taskSummaryBasicData(taskID: "dy65vko",priority: "Normal",location: "Mylapore"),
-     taskSummaryBasicData(taskID: "dy65vko",priority: "Normal",location: "Mylapore"),
-     taskSummaryBasicData(taskID: "dy65vko",priority: "Normal",location: "Mylapore")
-     
-    ]
+    
     
 }
